358f723197cfb9005edfb1f81f1e2d68bc9b9f27
vamsikolusu <vkolusu27@gmail.com>
2022-11-29 16:43:29 -0600

    added security

--- models/iceCream.js
+++ models/iceCream.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose") 
 const iceCreamSchema = mongoose.Schema({ 
-    icecream_brand: String, 
-    icecream_flavour: String, 
-    calories: Number 
+    icecream_brand: {type: String,required: [true, 'Brand of the Ice Cream cannot be empty']}, 
+    icecream_flavour: {type: String,required: [true, 'Flavour of the Ice Cream cannot be empty']}, 
+    calories: {type: Number,required: [true, 'Calories of the Ice Cream cannot be empty']} 
 }) 
 
  
--- routes/iceCream.js
+++ routes/iceCream.js
@@ -17,11 +17,11 @@ const secured = (req, res, next) => {
 router.get('/', iceCream_controlers.iceCream_view_all_Page );
 router.get('/iceCream/:id', iceCream_controlers.iceCream_detail );
 /* GET detail iceCream page */ 
-router.get('/detail', iceCream_controlers.iceCream_view_one_Page);
+router.get('/detail',secured, iceCream_controlers.iceCream_view_one_Page);
 /* GET create iceCream page */ 
 router.get('/create', iceCream_controlers.iceCream_create_Page);
 /* GET create update page */ 
 router.get('/update',secured, iceCream_controlers.iceCream_update_Page);
 /* GET delete icecream page */ 
-router.get('/delete', iceCream_controlers.iceCream_delete_Page); 
+router.get('/delete',secured, iceCream_controlers.iceCream_delete_Page); 
 module.exports = router;
