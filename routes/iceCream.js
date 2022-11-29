54bee1ced302aea5b2c8dfbba605789da11ce621
vamsikolusu <vkolusu27@gmail.com>
2022-11-29 14:01:29 -0600

    First

--- routes/iceCream.js
+++ routes/iceCream.js
@@ -1,6 +1,16 @@
 var express = require('express');
 const iceCream_controlers= require('../controllers/iceCream'); 
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on 
+//or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  }
 
 /* GET home page. */
 
@@ -11,7 +21,7 @@ router.get('/detail', iceCream_controlers.iceCream_view_one_Page);
 /* GET create iceCream page */ 
 router.get('/create', iceCream_controlers.iceCream_create_Page);
 /* GET create update page */ 
-router.get('/update', iceCream_controlers.iceCream_update_Page);
+router.get('/update',secured, iceCream_controlers.iceCream_update_Page);
 /* GET delete icecream page */ 
 router.get('/delete', iceCream_controlers.iceCream_delete_Page); 
 module.exports = router;
